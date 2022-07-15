3ee8b2a578beacf4a7e4ad07d4def75bf7f9587b
Michael Jennings <michaeljenningsai@gmail.com>
2022-07-15 21:53:58 +1000

    2nd program

---
+++ SemanticError.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SemanticError
+{
+ public static void main(String[] args)
+ {
+ int height=0, width=0, depth=0;
+ System.out.println("Enter the height, width, and depth of");
+ System.out.println("a box and I will compute the volume.");
+ Scanner keyboard = new Scanner(System.in);
+ height = keyboard.nextInt();
+ width = keyboard.nextInt();
+ depth = keyboard.nextInt();
+ int volume = height * width * depth;
+ System.out.println("The volume is " + volume);
+ }
+}
