76e3df03b7a80498b211be71d8b3867637243127
michaeljenningsai <michaeljenningsai@gmail.com>
2022-07-25 16:37:16 +1000

    test2

---
+++ Average3.java
@@ -0,0 +1,12 @@
+public class Average3 {
+    public static void main(String[] args)
+    {
+        int num1 = 75;
+        int num2 = 137;
+        int num3 = 265;
+        double average = (num1 + num2 + num3)/3;
+        
+        System.out.print("The average of " + num1);
+        System.out.println(", " + num2 + " and " + num3 + " is " + average);   
+    }
+}
--- Salary.java
+++ Salary.java
@@ -3,13 +3,14 @@ public class Salary
 {
     public static void main (String[] args)
     {
-        Scanner console = new Scanner(System.in);
-        double normal,extra,total;
-        System.out.println("Please Enter number of normal hours: ");
-        normal = console.nextDouble();
-        System.out.println("Please Enter number of extra hours: ");
-        extra = console.nextDouble();
-        total = 10*normal+15*extra;
-        System.out.println("Total salary is: "+total+"\n");
+        try (Scanner console = new Scanner(System.in)) {
+            double normal,extra,total;
+            System.out.println("Please Enter number of normal hours: ");
+            normal = console.nextDouble();
+            System.out.println("Please Enter number of extra hours: ");
+            extra = console.nextDouble();
+            total = 10*normal+15*extra;
+        System.out.println("Total salary is: "+ total +"\n");
+        }   
     }
 }
